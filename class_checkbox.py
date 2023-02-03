ec86f99cf32e5f5995450c4e9e87ec4fcc9fbaeb
JCWoodworker <jc@rilocalwoodworks.com>
2023-02-02 21:47:37 -0500

    re-formatting all files for consistency

--- class_checkbox.py
+++ class_checkbox.py
@@ -1,32 +1,16 @@
 import tkinter as tk
 
-
 class Checkbox:
-    def __init__(
-        self, frame, text, bg, fg, row, column, padx=None, pady=None, variable=any
-    ):
-        self.frame = frame
-        self.text = text
-        self.bg = bg
-        self.fg = fg
-        self.row = row
-        self.column = column
-        self.padx = padx
-        self.pady = pady
-        self.variable = variable
+  def __init__(self, frame, text, bg, fg, row, column, padx=None, pady=None, variable=any):
+    self.frame = frame
+    self.text = text
+    self.bg = bg
+    self.fg = fg
+    self.row = row
+    self.column = column
+    self.padx = padx
+    self.pady = pady
+    self.variable = variable
 
-        self.checkbutton = tk.Checkbutton(
-            self.frame,
-            text=self.text,
-            bg=self.bg,
-            fg=self.fg,
-            font=("Arial", 15),
-            variable=self.variable,
-        )
-        self.checkbutton.grid(
-            row=self.row,
-            column=self.column,
-            padx=self.padx,
-            pady=self.pady,
-            sticky="ns",
-        )
+    self.checkbutton = tk.Checkbutton(self.frame, text=self.text, bg=self.bg, fg=self.fg, font=("Arial", 15), variable=self.variable)
+    self.checkbutton.grid(row=self.row, column=self.column, padx=self.padx, pady=self.pady, sticky="ns")
--- class_note.py
+++ class_note.py
@@ -1,8 +1,7 @@
 import datetime
 
-
 class Note:
-    def __init__(self, title, body):
-        self.title = title
-        self.body = body
-        self.timestamp = datetime.datetime.now()
+  def __init__(self, title, body):
+    self.title = title
+    self.body = body
+    self.timestamp = datetime.datetime.now()
